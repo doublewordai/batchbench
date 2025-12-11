78bb0419b4b5a952c74894bae9b73b308bf7c7ea
jamesdborin <james.dborin@gmail.com>
2025-12-11 10:58:49 +0000

    rushil sharing

--- docker/run_vllm.sh
+++ docker/run_vllm.sh
@@ -11,3 +11,13 @@ vllm serve \
     --trust-remote-code \
     --host 0.0.0.0 \
     --port 8000
+
+VLLM_FLASHINFER_MOE_BACKEND=throughput vllm serve \
+    --model Qwen/Qwen3-30B-A3B-Instruct-2507 \
+    --gpu-memory-utilization 0.9 \
+    --trust-remote-code \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --enable-expert-parallel \
+    --tensor-parallel-size 1 \
+    --data-parallel-size 2
